49b8d42ad65f748922d1734924582c9c172b0e0b
sknt0730 <zm19568@josai.ac.jp>
2021-12-13 17:32:20 +0000

    0808

---
+++ ex06-10.js
@@ -0,0 +1,8 @@
+function imgchange1() {
+    document.getElementById('logo').src =
+    "image/keiei_small.jpg";
+}
+function imgchange2() {
+    document.getElementById('logo').src =
+    "image/josai.png";
+}
